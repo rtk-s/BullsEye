b942ed219fb7940b7978f13b14326aca75c24180
rtk-s <nikolay.malofeev@gmail.com>
2015-12-01 15:15:44 +0300

    Initial Commit

---
+++ BullsEye/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BullsEye
+//
+//  Created by Nikolay on 01.12.15.
+//  Copyright © 2015 nut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
